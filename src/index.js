05ade1f5b97d4dca47a4db5a54468544da4aa970
Kostiantyn <kostya20022210@gmail.com>
2025-01-29 21:07:27 +0200

    Simplify if statement, remove redundant

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@
  * @returns {number} factorial of the number
  */
 function calculateFactorial(number) {
-    if (typeof number !== 'number' || number < 0 || Number.isInteger(number) === false || Number.isNaN(number) || !Number.isFinite(number)) {
+    if (typeof number !== 'number' || number < 0 || Number.isNaN(number) || !Number.isInteger(number) || !Number.isFinite(number)) {
         return NaN;
     }
     
@@ -19,14 +19,10 @@ function calculateFactorial(number) {
 
 let userInputNumber = parseInt(prompt('Enter a number to calculate its factorial: '));
 
-if (Number.isNaN(userInputNumber)) {
+let factorial = calculateFactorial(userInputNumber);
+
+if (Number.isNaN(factorial)) {
     alert(`${userInputNumber} is not a valid number, please provide a valid number`);
 } else {
-    let factorial = calculateFactorial(userInputNumber);
-
-    if (Number.isNaN(factorial)) {
-        alert(`${userInputNumber} is not a valid number, please provide a valid number`);
-    } else {
-        alert(`The factorial of ${userInputNumber} is ${factorial}`);
-    }
+    alert(`The factorial of ${userInputNumber} is ${factorial}`);
 }
