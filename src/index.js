142a044bab727c991c82623b5502b0e2fbe1ebb8
Kostiantyn <kostya20022210@gmail.com>
2025-01-30 13:19:50 +0200

    Update comments

--- src/index.js
+++ src/index.js
@@ -18,13 +18,13 @@ function calculateFactorial(number) {
 }
 
 // Test cases
-console.log(calculateFactorial(10)); // 3628800
+console.log(calculateFactorial(10)); // 3_628_800
 console.log(calculateFactorial(6)); // 720
 console.log(calculateFactorial(5)); // 120
 console.log(calculateFactorial(0)); // 1
 
 // Faulty test cases
-console.log(calculateFactorial('5')); // NaN
-console.log(calculateFactorial(-5)); // NaN
-console.log(calculateFactorial(5.5)); // NaN
-console.log(calculateFactorial(Infinity)); // NaN
+console.log(calculateFactorial('5')); // NaN (Not a number)
+console.log(calculateFactorial(-5)); // NaN (Negative number)
+console.log(calculateFactorial(5.5)); // NaN (Not an integer)
+console.log(calculateFactorial(Infinity)); // NaN (Infinite number)
